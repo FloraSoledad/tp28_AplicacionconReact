f9978ade521d9da16f3f9604f7934c166bf24ea2
FloraSoledad <vargas.flora@hotmail.com>
2023-01-03 03:55:14 -0300

    aplicacion con read terminado

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="sticky-footer bg-white">
+    <div className="container my-auto">
+      <div className="copyright text-center my-auto">
+        <span>Copyright &copy; Dashboard 2021</span>
+      </div>
+    </div>
+  </footer>
   )
 }
